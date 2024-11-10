250879e40209b7e28db2847ea093c13b27034b7d
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2024-11-10 01:24:36 +0100

    update

--- src/main/java/net/ensah/demo/repository/AppUserRepository.java
+++ src/main/java/net/ensah/demo/repository/AppUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(path = "users")
+@RepositoryRestResource(path = "utilisateurs")
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 }
